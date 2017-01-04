7d18eac6d66f6cc84a027fc4ea10a842472e8296
grooveymo <ingeniumsoft@gmail.com>
2017-01-04 10:59:36 +0000

    first commit

---
+++ app/index.js
@@ -0,0 +1,13 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+//var routes = require('./config/routes');
+
+ReactDOM.render(
+    <h1>Hello from React!</h1> ,
+    document.getElementById('root')
+);
+
+//ReactDOM.render(
+//    routes,
+//    document.getElementById('root')
+//);
