9eeaa2100e1052d608436d7558510f5f93fe7c7f
grooveymo <ingeniumsoft@gmail.com>
2017-01-06 08:05:34 +0000

    implemented first cut of view all Lists

--- app/components/ViewLists.js
+++ app/components/ViewLists.js
@@ -1,18 +1,39 @@
 var React = require('react');
 
+//<div className="container">
+//    <div className="well col-sm-12 text-left">
+//        <h3>View Lists Page</h3>
+//        <p>
+//            This page will allow you to view Lists
+//        </p>
+//
+//    </div>
+//</div>
+
 var ViewLists = React.createClass({
     render : function(){
         return (
             <div className="container">
-                    <div className="well col-sm-12 text-left">
-                        <h3>View Lists Page</h3>
-                        <p>
-                            This page will allow you to view Lists
-                        </p>
-
-                    </div>
+                <table className="table">
+                    <thead>
+                    <tr>
+                        {this.props.tableHeaders.map(function(title) {
+                            return <th key={title}>{title}</th>;
+                        })}
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {this.props.todoLists.map(function(row, i) {
+                        return (
+                            <tr key={i}>
+                                <td>{row.title}</td>
+                                <td>{row.todos.length}</td>
+                            </tr>
+                        );
+                    })}
+                    </tbody>
+                </table>
             </div>
-
         )
     }
 });
--- app/containers/ViewListsContainer.js
+++ app/containers/ViewListsContainer.js
@@ -6,17 +6,25 @@ var RestUtils = require('../utils/RestUtils');
 //TODO - look at using existing react table component - https://facebook.github.io/fixed-data-table/
 var ViewListsContainer = React.createClass({
     getInitialState : function (){
+        return {
+            isLoading : true,
+            todoLists : [],
+            tableHeaders : ['Title', '#Items']
+        }
+    },
+    componentDidMount : function (){
         RestUtils.getAllLists()
             .then(function(response){
                 console.log('View All TodoList => ' + JSON.stringify(response.data));
-                return response.data;
-            });
-
-        return null;
+                this.setState({
+                    todoLists : response.data,
+                    isLoading : false
+                });
+            }.bind(this));
     },
     render : function(){
         return (
-            <ViewLists />
+            <ViewLists todoLists={this.state.todoLists} tableHeaders={this.state.tableHeaders}/>
         )
     }
 });
